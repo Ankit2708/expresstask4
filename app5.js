4c4baf1f04cf69f754c414a772fbc021d8478295
Ankit2708 <shivhare.ankit476@gmail.com>
2022-04-11 21:15:09 +0530

    express2comit

--- app5.js
+++ app5.js
@@ -1,12 +1,26 @@
 //const http=require('http')
 const express=require('express');
-const app=express();
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();//Allows to to go next middleware in line
+const app=express();//express is exported as a function in node
+const bodyParser=require('body-parser');//importing body parser
+
+app.use(bodyParser.urlencoded({extended:false}))//body parser and it should always happen 
+app.use('/add-product',(req,res,next)=>{//specific routes for specific request
+    
+    res.send('<form action="/product" method="post"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button>');//Allows to to go next middleware in line
+    
 })
-app.use((req,res,next)=>{
-    console.log('In another middleware');
+app.post('/product',(req,res,next)=>{//this will trigger only post request
+    console.log(req.body);
+
+    res.redirect('/');
+})
+// app.use('/product',(req,res,next)=>{//this will trigger both get and post request
+//     console.log(req.body);
+
+//     res.redirect('/');//it will redirect to the page
+// })
+app.use('/',(req,res,next)=>{
+    
     res.send('<h1>Hello from Express</h1>');
     //res.send({key1:value});
 })
