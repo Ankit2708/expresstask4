843447bbcec83c94fd02bcb9fd10d0ad590aeb71
Ankit2708 <shivhare.ankit476@gmail.com>
2022-04-14 12:17:03 +0530

    controllercomit1

--- routes/admin.js
+++ routes/admin.js
@@ -1,26 +1,18 @@
-//app6 is routing
-const path=require('path');
-const express=require('express');
-const router=express.Router();//router function
-const rootdir=require('../util/path')//importing path
-//admin get request
-router.get('/add-product',(req,res,next)=>{//specific routes for specific request
-    
-    //res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button>');//Allows to to go next middleware in line
-    res.sendFile(path.join(rootdir,'viewsscreen','add-product.html'))
-})
-//admin post request  ,,same path can be used if methhods are different
-router.post('/add-product',(req,res,next)=>{//this will trigger only get request
-    console.log(req.body);
+const path = require('path');
 
-    //res.redirect('/admin/contact');
-})
-router.get('/contact',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../','viewsscreen','contact.html'));
-    //res.redirect('/admin/success')
-    next();
-})
-router.post('/admin/success',(req,res,next)=>{
-    res.send('<h1>Form submission succesful</h1>');
-})
+const express = require('express');
+
+//const rootDir = require('../util/path');Not required now
+const productsController=require('../controllers/products')//going just 1 level upwards
+
+const router = express.Router();
+
+
+
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);//getaddproduct is passed here as reference
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
+
 module.exports=router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
-const path=require('path');//gives absolute path of project folder
-const express=require('express');
-const router=express.Router();
-router.get('/',(req,res,next)=>{
-    
-    //res.send('<h1>Hello from Express</h1>');
-    res.sendFile(path.join(__dirname,'../','viewsscreen','shop.html'))//it will join the pathof html and js
-    //path join detects os and chose correct path for it
-})
-module.exports=router;
+const path = require('path');
+
+const express = require('express');
+
+const productsController=require('../controllers/products');
+
+const router = express.Router();
+
+router.get('/', productsController.getProducts);
+
+module.exports = router;
