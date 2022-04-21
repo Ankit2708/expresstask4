d006138d821987b905c3ed4d1cdc8504e6a5da28
Ankit2708 <shivhare.ankit476@gmail.com>
2022-04-22 00:54:06 +0530

    addtocartcomit

--- models/product.js
+++ models/product.js
@@ -1,41 +1,48 @@
-const fs=require('fs');
-const path=require('path');
-const p=path.join
-    (path.dirname(process.mainModule.filename),
-    'data',
-    'products.json');
-const getProductsFromFile=cb=>{
-    
-        fs.readFile(p,(err,FileContent)=>{
-            if(err){
-                return cb([])
-            }else{
-                cb(JSON.parse(FileContent));
-            }
-            
-        })
+const fs = require('fs');
+const path = require('path');
 
-}
-const products=[];
-module.exports=class Product{
-    constructor(t){
-        this.title=t;
-    }
-    save(){
-        getProductsFromFile(products=>{
-            products.push(this);
-            fs.writeFile(p,JSON.stringify(products),err=>{
-                console.log(err)
-            })
-        })
-        
-            
-            
-       
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-       
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    static fetchAll(cb){
-        getProductsFromFile(cb)
-    }
-}
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
+  }
+};
